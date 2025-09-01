c70c4e5552ffee066b457e1c75e8feba834327ae
uthomasz <yothomas2011.tye@gmail.com>
2025-09-01 01:21:53 -0300

    cambia import cdn supabase

--- js/api.js
+++ js/api.js
@@ -1,4 +1,5 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/dist/umd/supabase.min.js';
+//import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.45.4/dist/umd/supabase.min.js';
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
 import { SUPABASE_CONFIG } from './config.js';
 
 // Inicializar el cliente de Supabase una sola vez
